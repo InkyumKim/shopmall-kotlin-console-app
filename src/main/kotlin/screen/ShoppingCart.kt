aec8fac2afd4b9ed3bac81caf19a36d90a8a8182
InKyum <dlsrua0330@naver.com>
2022-02-04 17:55:24 +0900

    단계 4-3. 프로젝트 전체에서 사용하는 상수 분리

--- src/main/kotlin/screen/ShoppingCart.kt
+++ src/main/kotlin/screen/ShoppingCart.kt
@@ -1,5 +1,6 @@
 package screen
 
+import LINE_DEVIDER
 import data.CartItems
 
 //장바구니의 내용을 화면에 출력
@@ -13,8 +14,8 @@ class ShoppingCart {
                 products.keys.joinToString(
                     separator = ", \n",
                     prefix = """
-                        ***======================================***
-                                장바구니에 담긴 상품 목록입니다.
+                        $LINE_DEVIDER
+                        장바구니에 담긴 상품 목록입니다.
                                 
                     """.trimIndent()
                 ){ product -> "카테고리: ${product.categoryLabel} / 상품명 : ${product.name} / 수량 : ${products[product]}"
@@ -22,7 +23,7 @@ class ShoppingCart {
             )
         }else{
             println("""
-                장바구니에 담긴 상품이 없습니다.
+            장바구니에 담긴 상품이 없습니다.
             """.trimIndent())
         }
     }
--- src/main/kotlin/screen/ShoppingProductList.kt
+++ src/main/kotlin/screen/ShoppingProductList.kt
@@ -1,5 +1,6 @@
 package screen
 
+import LINE_DEVIDER
 import data.CartItems
 import data.Product
 import extensions.getNotEmptyInt
@@ -32,7 +33,7 @@ class ShoppingProductList {
 
         if(!categoryProducts.isNullOrEmpty()){
             println("""
-                ***================================================***
+                $LINE_DEVIDER
                      선택하신 [$selectedCategory] 카테고리 상품입니다.                
             """.trimIndent())
             /*
@@ -54,8 +55,8 @@ class ShoppingProductList {
 
     private fun showCartOption(categoryProducts: List<Product>, selectedCategory: String) {
         println("""
-            ***================================================***
-                   장바구니에 담을 상품 번호를 선택해 주세요.
+            $LINE_DEVIDER
+            장바구니에 담을 상품 번호를 선택해 주세요.
         """.trimIndent())
 
         //상품 번호 입력수행
